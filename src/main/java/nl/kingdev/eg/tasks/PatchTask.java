b5556a46a0466607a69dcfda85facc5481afbda4
KingdevNL <jasper@kingdev.nl>
2019-02-26 18:30:43 +0100

    Update to new patch system

--- src/main/java/nl/kingdev/eg/tasks/PatchTask.java
+++ src/main/java/nl/kingdev/eg/tasks/PatchTask.java
@@ -1,5 +1,7 @@
 package nl.kingdev.eg.tasks;
 
+import com.cloudbees.diff.ContextualPatch;
+import com.cloudbees.diff.PatchException;
 import nl.kingdev.eg.EGPlugin;
 import nl.kingdev.eg.logger.Logger;
 import org.gradle.api.DefaultTask;
@@ -27,35 +29,48 @@ public class PatchTask extends DefaultTask {
             }
             Files.copy(resourceAsStream, patchFile.toPath());
 
+            ContextualPatch patch = ContextualPatch.create(patchFile, new File(project.getProjectDir(), "lib/Equilinox"));
+            patch.patch(false).forEach(patchReport -> {
+
+                if(patchReport.getStatus() == ContextualPatch.PatchStatus.Patched) {
+                    System.out.println("Patched: "+patchReport.getFile().getName());
+                }
+            });
+
+
         } catch (IOException e) {
             e.printStackTrace();
 
             throw new GradleException("Failed to copy patch file!");
-        }
-
-        String patchCommand = "";
-
-        if(EGPlugin.OS.contains("win")) {
-            patchCommand = "cmd /c \"cd lib/Equilinox && git apply  --directory=lib/Equilinox game.patch \"";
-        } else {
-            patchCommand = "bash -c \"pwd && cd lib/Equilinox && git apply  --directory=lib/Equilinox game.patch \"";
-        }
-        System.out.println("PatchCommand: " + patchCommand);
-
-        try {
-            Runtime runtime = Runtime.getRuntime();
-            Process process = runtime.exec(patchCommand);
-            final InputStream in = process.getInputStream();
-            int ch;
-            while ((ch = in.read()) != -1) {
-                System.out.print((char) ch);
-            }
-            in.close();
-            Logger.info("Patching is done.");
-
-        } catch (Exception e) {
+        } catch (PatchException e) {
             e.printStackTrace();
         }
+
+
+        //
+//        String patchCommand = "";
+//
+//        if(EGPlugin.OS.contains("win")) {
+//            patchCommand = "cmd /c \"cd lib/Equilinox && git apply  --directory=lib/Equilinox game.patch \"";
+//        } else {
+//            patchCommand = "bash -c \"pwd && cd lib/Equilinox && git apply  --directory=lib/Equilinox game.patch \"";
+//        }
+//        System.out.println("PatchCommand: " + patchCommand);
+//
+//        try {
+//            Runtime runtime = Runtime.getRuntime();
+//            Process process = runtime.exec(patchCommand);
+//            final InputStream in = process.getInputStream();
+//            int ch;
+//            while ((ch = in.read()) != -1) {
+//                System.out.print((char) ch);
+//            }
+//            in.close();
+//            Logger.info("Patching is done.");
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
 
 
